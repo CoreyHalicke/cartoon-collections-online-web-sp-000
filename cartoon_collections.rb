fba1b2f2fcf78af9226a1d5937acc5376fcff776
Corey H <c.l.halicke@gmail.com>
2019-03-11 01:11:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
-  
+  names.each do |name|
+    purs name
+  end
 end
 
 def summon_captain_planet# code an argument here
